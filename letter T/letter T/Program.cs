b93660f6745afdeda5d3e9e344a0978a7786fcba
sameh <39744797+sameh20130@users.noreply.github.com>
2020-12-23 18:47:04 +0200

    Add files via upload

---
+++ letter T/letter T/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace letter_T
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row, column;
+
+            for (row = 0; row <= 6; row++)
+            {
+                for (column = 0; column <= 6; column++)
+                {
+                    if (column == 3 || (row == 0 && column > 0 && column < 6))
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.Write("\n");
+            }
+            Console.Read();
+        }
+    }
+}
